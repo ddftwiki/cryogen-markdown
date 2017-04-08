5e4de4930a6860690696e1bfa4eb8703efd2478d
Brandon Adams <emidln@gmail.com>
2017-04-07 22:54:12 -0500

    fork for ddft.wiki

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cryogen-markdown "0.1.6"
-  :description "Markdown parser for Cryogen"
-  :url "https://github.com/cryogen-project/cryogen-markdown"
+(defproject wiki.ddft/cryogen-markdown "0.1.7-SNAPSHOT"
+  :description "Markdown parser for Cryogen (forked for ddft.wiki)"
+  :url "https://github.com/ddftwiki/cryogen-markdown"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
